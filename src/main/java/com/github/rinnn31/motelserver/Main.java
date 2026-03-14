800d62c00420100be6a9037ebedaa21da5ccc185
rinnn31 <damquangphongthdk@gmail.com>
2026-03-14 16:48:44 +0700

    update project and docker configuration

---
+++ src/main/java/com/github/rinnn31/motelserver/Main.java
@@ -0,0 +1,13 @@
+package com.github.rinnn31.motelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/java/com/github/rinnn31/motelserver/config/TwilioProperties.java
@@ -0,0 +1,15 @@
+package com.github.rinnn31.motelserver.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+
+@ConfigurationProperties("sms.twilio")
+public record TwilioProperties(
+    String accountSid,
+    String authToken,
+    String fromNumber
+) {
+    @ConstructorBinding
+    public TwilioProperties {
+    }
+}
